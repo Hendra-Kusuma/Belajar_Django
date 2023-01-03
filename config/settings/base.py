aea676573b15f1186087af7cff55c9af6d3bd655
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2023-01-03 08:03:09 +0700

    update 03-01-2022, 08.02 AM

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -84,6 +84,14 @@ AUTHENTICATION_BACKENDS = [
 ]
 # site i
 SITE_ID = 1
+
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
+# Apply Customs User
+AUTH_USER_MODEL = 'users.Users'
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
