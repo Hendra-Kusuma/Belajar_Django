f85ca017769a10bf7616a1ad1f9ea9722f71ca42
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2023-01-19 14:27:46 +0700

    update 19-01-2022, 14.26 PM

---
+++ static/js/home.js
@@ -0,0 +1,31 @@
+let currentScrollPosition = 0;
+let currentScrollAmount = 320;
+const sCont = document.querySelector('.storys-container');
+const hScroll = document.querySelector('.horizontal-scroll');
+
+// Scroll handle
+
+const btnScrolleft = document.querySelector('#btn-scroll-left');
+const btnScrolRight = document.querySelector('#btn-scroll-right');
+
+btnScrolleft.style.opacity = '0';
+
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+
+function scrollHorizontally(val) {
+    currentScrollPosition += (val * currentScrollAmount);
+  if (currentScrollPosition >= 0) {
+    currentScrollPosition = 0;
+    btnScrolleft.style.opacity = '0';
+  } else {
+    btnScrolleft.style.opacity = '1';
+  }
+  if (currentScrollPosition <= maxScroll) {
+    currentScrollPosition = maxScroll;
+    btnScrolRight.style.opacity = '0';
+  } else {
+    btnScrolRight.style.opacity = '1';
+  }
+
+  sCont.style.left = currentScrollPosition + 'px';
+}
